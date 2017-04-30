de2f8430a5c6b52024e04faeb97023af5194a4ed
Matej Puk <puk.matej@gmail.com>
2017-04-30 19:55:54 +0200

    test api; skeleton for real api

--- aies.py
+++ aies.py
@@ -9,21 +9,35 @@ def main():
 	except Exception, e:
 		return(str(e))
 
-@app.route('/getTemp')
-def getTemp():
+@app.route('/getTempTest')
+def getTempTest():
     try:
         temp = randint(1, 100)
         return jsonify(temp)
     except Exception, e:
         return(str(e))
 
-@app.route('/getHum')
-def getHum():
+@app.route('/getHumTest')
+def getHumTest():
     try:
         hum = randint(1, 100)
         return jsonify(hum)
     except Exception, e:
         return(str(e))
+
+@app.route('/getTemp')
+def getTemp():
+	try:
+		pass
+	except Exception, e:
+		return(str(e))
+
+@app.route('/getHum')
+def getHum():
+	try:
+		pass
+	except Exception, e:
+		return(str(e))
 
 if __name__ == "__main__":
     app.run()
