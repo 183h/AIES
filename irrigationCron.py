d854159e2f39d6dd2aba89ba556db7ca66ad78fe
Matej Puk <puk.matej@gmail.com>
2017-05-03 23:43:49 +0200

    added sleep after serial connect due to arduino reset

--- irrigationCron.py
+++ irrigationCron.py
@@ -6,12 +6,14 @@ from re import findall
 device = findall('ttyUSB[0-9]*', check_output(["ls","/dev"]))[0]
 s = Serial('/dev/' + device, 9600)
 
+sleep(5)
+
 command='valve_on'
 s.write(command.encode())
-#status = s.readline().decode('ascii').strip()
+status = s.readline().decode('ascii').strip()
 
-sleep(5)
+sleep(10)
 
 command='valve_off'
 s.write(command.encode())
-#status = s.readline().decode('ascii').strip()
+status = s.readline().decode('ascii').strip()
